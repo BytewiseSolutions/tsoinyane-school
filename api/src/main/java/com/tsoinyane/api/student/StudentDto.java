7ceae8dd1a499097967ebda5bb23d9bb1ba93d96
LebohangMonamane <monamane.lebohang45@gmail.com>
2026-03-29 18:05:33 +0200

    Done

--- api/src/main/java/com/tsoinyane/api/student/StudentDto.java
+++ api/src/main/java/com/tsoinyane/api/student/StudentDto.java
@@ -1,6 +1,24 @@
 package com.tsoinyane.api.student;
 
 import com.tsoinyane.api.common.BaseDto;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
 public class StudentDto extends BaseDto {
+
+    private Long userId;
+    private String userFullName;
+    private String userEmail;
+    private String userPhone;
+
+    private Long schoolId;
+    private String schoolName;
+
+    private String grade;
+    private String studentNumber;
 }
